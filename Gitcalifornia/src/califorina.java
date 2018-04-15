da1b8da3f596fc60beb8a7f82c05d5cd1f37c669
preethikishor <30550110+preethikishor@users.noreply.github.com>
2018-04-15 13:19:28 +0530

    Update califorina.java

--- Gitcalifornia/src/califorina.java
+++ Gitcalifornia/src/califorina.java
@@ -2,8 +2,8 @@ public class califorina
 {
 public static void main(String[] args) 
 {
-System.out.println("preethi moved to *******");
-System.out.println(" moveing to califorina fgfgfg*****");
+System.out.println("preethi moved to ");
+System.out.println(" moveing to califorina fgfgfg");
 System.out.println(" moveing to califorina by plane");
 }
 }
