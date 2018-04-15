cbabf1d444b2cd97ef609c5d6ab30eb63d7a6df2
preethikishor <preethidj@gmail.com>
2018-04-15 13:22:50 +0530

    8888

--- Gitcalifornia/src/califorina.java
+++ Gitcalifornia/src/califorina.java
@@ -4,6 +4,6 @@ public static void main(String[] args)
 {
 System.out.println("preethi moved to *******");
 System.out.println(" moveing to califorina fgfgfg*****");
-System.out.println(" moveing to califorina by plane");
+System.out.println(" moveing to califorina by plane *****");
 }
 }
