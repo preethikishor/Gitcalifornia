39107a535f75f79b0d237f55515e6bca0f9b1950
preethikishor <30550110+preethikishor@users.noreply.github.com>
2018-04-15 11:53:27 +0530

    fgfg

--- Gitcalifornia/src/califorina.java
+++ Gitcalifornia/src/califorina.java
@@ -2,7 +2,7 @@ public class califorina
 {
 public static void main(String[] args) 
 {
-System.out.println("***** i moved to califorina");
+
 System.out.println("preethi moved to ");
 System.out.println(" moveing to califorina fgfgfg*****");
 System.out.println(" moveing to califorina by plane");
