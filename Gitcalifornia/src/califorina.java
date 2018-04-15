b7b6837283df572ef09e11af072d8307c5671735
preethikishor <preethidj@gmail.com>
2018-04-15 13:17:29 +0530

    5555

--- Gitcalifornia/src/califorina.java
+++ Gitcalifornia/src/califorina.java
@@ -2,13 +2,6 @@ public class califorina
 {
 public static void main(String[] args) 
 {
-<<<<<<< HEAD
-System.out.println("***** i moved to califorina");
-System.out.println("preethi moved to ");
-System.out.println(" moveing to rtrt fgfgfg");
-=======
->>>>>>> 227a2b10894258299ee485ac7a9a40e7553f7c24
-
 System.out.println("preethi moved to *******");
 System.out.println(" moveing to califorina fgfgfg*****");
 System.out.println(" moveing to califorina by plane");
