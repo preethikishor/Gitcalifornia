a10b98bada0da862953d4f83acbd6561beabb3c7
preethikishor <preethidj@gmail.com>
2018-05-22 18:23:31 +0530

    DFDF

--- Gitcalifornia/src/califorina.java
+++ Gitcalifornia/src/califorina.java
@@ -3,7 +3,7 @@ public class califorina
 public static void main(String[] args) 
 {
 System.out.println("preethi moved to ");
-System.out.println(" moveing to califorina fgfgfg");
-System.out.println(" moveing to califorina by plane");
+System.out.println("moveing to califorina fgfgfg");
+System.out.println("moveing to califorina by plane");
 }
 }
