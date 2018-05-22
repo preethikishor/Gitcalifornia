f31213588c03ff4198a147244f015e1b53cab009
preethikishor <preethidj@gmail.com>
2018-05-22 21:40:44 +0530

    dfdf

--- Gitcalifornia/src/cal.java
+++ Gitcalifornia/src/cal.java
@@ -3,18 +3,8 @@ public class cal {
 
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
+
 		System.out.println("california moving");
 		
-=======
-		System.out.println("california moving 222");
-<<<<<<< HEAD
-		System.out.println("california moving 111 ###");
->>>>>>> 6ae585950c33cd38506c8231cedecc184a2b3c4c
-
 	}
-=======
-			}
->>>>>>> f283cfe7297f9d86961c1407a521e0263958eca2
-
 }
--- Gitcalifornia/src/califorina.java
+++ Gitcalifornia/src/califorina.java
@@ -3,9 +3,6 @@ public class califorina
 public static void main(String[] args) 
 {
 
-System.out.println("preethi moved to dfdf ");
 System.out.println("preethi moved to california ");
-
-
 }
 }
