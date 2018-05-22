f24d2c2fd85fb997c42c6a9017ffa7a177faeae9
preethikishor <preethidj@gmail.com>
2018-05-22 23:27:39 +0530

    first commit

--- Gitcalifornia/src/calcutta.java
+++ Gitcalifornia/src/calcutta.java
@@ -3,7 +3,7 @@ public class calcutta {
 
 	public static void main(String[] args) {
 		System.out.println("ON THE WAY TO CALCUTTA");	
-		System.out.println("reached THE WAY TO CALCUTTA");		
+		System.out.println("@@@ reached THE WAY TO CALCUTTA");		
 
 }
 }
