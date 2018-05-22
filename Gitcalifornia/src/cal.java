97272546e95834837740540198150b85df2733a2
preethikishor <30550110+preethikishor@users.noreply.github.com>
2018-05-22 18:35:25 +0530

    Update cal.java

--- Gitcalifornia/src/cal.java
+++ Gitcalifornia/src/cal.java
@@ -4,7 +4,7 @@ public class cal {
 	public static void main(String[] args) {
 		
 		System.out.println("california moving");
-		System.out.println("california moving DDFDF");
+		System.out.println("california moving DDFDF 111");
 
 	}
 
