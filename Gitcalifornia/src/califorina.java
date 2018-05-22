4e6475ee391e14ef0abfc31b8cd136e33e34c75c
preethikishor <30550110+preethikishor@users.noreply.github.com>
2018-05-22 19:17:47 +0530

    dfdfdf

--- Gitcalifornia/src/califorina.java
+++ Gitcalifornia/src/califorina.java
@@ -3,7 +3,7 @@ public class califorina
 public static void main(String[] args) 
 {
 
-System.out.println("preethi moved to dfdfdfdfdf ");
+System.out.println("preethi moved to dfdf ");
 System.out.println("preethi moved to california ");
 
 
