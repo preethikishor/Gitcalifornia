72d4276d4eadbb57ff4f4f7017061a2ae1edc30b
preethikishor <30550110+preethikishor@users.noreply.github.com>
2018-05-23 21:20:48 +0530

    preethi removed

--- Gitcalifornia/src/califorina.java
+++ Gitcalifornia/src/califorina.java
@@ -2,7 +2,7 @@ public class califorina
 {
 public static void main(String[] args) 
 {
-System.out.println("preethi moved to california ");
+
 System.out.println("meera moved to california ");
 System.out.println("sai moved to california ");
 System.out.println("preethi123 moved to california ");
