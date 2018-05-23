de9cbb6e3d85704e735cf0528934b8e731cdfd8b
preethikishor <30550110+preethikishor@users.noreply.github.com>
2018-05-23 22:01:39 +0530

    88

--- Gitcalifornia/src/califorina.java
+++ Gitcalifornia/src/califorina.java
@@ -6,5 +6,6 @@ public static void main(String[] args)
 System.out.println("meera moved to california ");
 System.out.println("sai moved to california ");
 System.out.println("preethi123 moved to california ");
+System.out.println("***** moved to california ");
 }
 }
