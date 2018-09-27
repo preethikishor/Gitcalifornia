bade8aef48f4ebcacefa17829e79883cfea01471
preethikishor <preethidj@gmail.com>
2018-09-27 21:39:50 +0530

    WWWW

--- Gitcalifornia/src/califorina.java
+++ Gitcalifornia/src/califorina.java
@@ -3,10 +3,7 @@ public class califorina
 public static void main(String[] args) 
 {
 
-System.out.println("martha s moved to california ");
 
-System.out.println("preethi d moved to california ");
-  
 System.out.println("sindu R moved to california ");
 
 System.out.println("meera p moved to california ");
