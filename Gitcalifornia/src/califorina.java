d4a400e03a2815632fbfa0b9252a9742afed946b
preethikishor <preethidj@gmail.com>
2018-09-27 21:18:07 +0530

    DDD

--- Gitcalifornia/src/califorina.java
+++ Gitcalifornia/src/califorina.java
@@ -3,7 +3,6 @@ public class califorina
 public static void main(String[] args) 
 {
 System.out.println("sindu moved to california ");
-System.out.println("marthaa moved to california ");
-
+System.out.println("martha s moved to california ");
 }
 }
