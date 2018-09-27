699a9df1035f51e63f2c2a16d2b715c813d81a7e
preethikishor <30550110+preethikishor@users.noreply.github.com>
2018-09-27 21:02:54 +0530

    Update califorina.java

--- Gitcalifornia/src/califorina.java
+++ Gitcalifornia/src/califorina.java
@@ -5,6 +5,6 @@ public static void main(String[] args)
 
 System.out.println("meera moved to california ");
 System.out.println("sai moved to california ");
-
+System.out.println("martha moved to california ");
 }
 }
