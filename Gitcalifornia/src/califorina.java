2d39055824b2f1503a89df6bc8001366e0ed9325
preethikishor <preethidj@gmail.com>
2018-09-27 22:25:18 +0530

    moved

--- Gitcalifornia/src/califorina.java
+++ Gitcalifornia/src/califorina.java
@@ -3,12 +3,9 @@ public class califorina
 public static void main(String[] args) 
 {
 
-
 System.out.println("sindu  moved to california ");
-
 System.out.println("meera  moved to california ");
-
 System.out.println("preethi  moved to california ");
-
+System.out.println("ABC  moved to california ");
 }
 }
