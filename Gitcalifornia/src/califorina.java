f72c658ab1b4817873da3171cbc52314f3505e74
preethikishor <preethidj@gmail.com>
2019-03-12 23:24:59 +0530

    sss

--- Gitcalifornia/src/califorina.java
+++ Gitcalifornia/src/califorina.java
@@ -3,10 +3,8 @@ public class califorina
 public static void main(String[] args) 
 {
 
-
-
 System.out.println("123  moved to california ");
-
+System.out.println("125  moved to california ");
 
 }
 }
