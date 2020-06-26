630bc19556aca03b9ea85e412395b2779216feff
preethikishor <30550110+preethikishor@users.noreply.github.com>
2020-06-26 22:45:34 +0530

    Update calcutta.java

--- Gitcalifornia/src/calcutta.java
+++ Gitcalifornia/src/calcutta.java
@@ -5,7 +5,7 @@ public class calcutta {
 	{
 		
 		System.out.println("@@@ reached THE WAY TO CALCUTTA @@@@@");	
-		
+		System.out.println("$$$$ reached THE going TO preethi);
 		
 		System.out.println("$$$$ reached THE WAY TO preethi);
 }
